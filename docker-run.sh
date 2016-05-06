bbdc1ab5ad29b7cb2468064ec355365b0198953f
Stefan Thies <stefan.thies@sematext.com>
2016-05-06 17:21:12 +0200

    expose SPM_RECEIVER_URL and EVENTS_RECEIVER_URL for on-prem

--- docker-run.sh
+++ docker-run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 export SPM_AGENT_CONFIG_FILE=/etc/sematext/sematext-agent-nginx.config
+export SPM_RECEIVER_URL=${SPM_RECEIVER_URL:-https://spm-receiver.sematext.com:443/receiver/v1/_bulk}
+export EVENTS_RECEIVER_URL=${EVENTS_RECEIVER_URL:-https://event-receiver.sematext.com}
 mkdir -p $(dirname $SPM_AGENT_CONFIG_FILE)
 printf '{
 	"tokens": {
@@ -7,7 +9,8 @@ printf '{
 	},
 	"nginx": {
 		"url": "%s"
-	}
-}' ${SPM_TOKEN} ${NGINX_STATS_URL} > $SPM_AGENT_CONFIG_FILE
+	},
+	"spmSenderBulkInsertUrl": "%s",
+}' ${SPM_TOKEN} ${NGINX_STATS_URL} ${SPM_RECEIVER_URL} > $SPM_AGENT_CONFIG_FILE
  
 sematext-agent-nginx --config /etc/sematext/sematext-agent-nginx.config 
